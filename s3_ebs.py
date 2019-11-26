659df5da69b2952bb7185bd0abaa14d6537dc8fa
Aaron Earle-Richardson <jakdaxter31@gmail.com>
2019-11-26 14:02:04 -0500

    sped up downloads by zipping and unzipping files

--- s3_ebs.py
+++ s3_ebs.py
@@ -4,6 +4,8 @@ import argparse
 import os
 import sys
 import botocore
+import tarfile
+import io
 from multiprocessing import Pool, current_process, cpu_count
 
 def get_parser(): #parses flags at onset of command
@@ -110,18 +112,13 @@ class Transfer():
 
 
         p2 = Pool()
-        for i, _ in enumerate(p2.imap(self.download_thread, keys, int(len(keys)/(cpu_count()*num_chunks))), 1):
+        sys.stderr.write("\n")
+        chunksize = max(int(len(keys)/(cpu_count()*num_chunks)),1)
+        for i, _ in enumerate(p2.imap(self.download_thread, keys, chunksize), 1):
             sys.stderr.write('\rDownloading Files: {0:.2%}'.format(i/len(keys)))
         p2.close()
         p2.join()
-        '''
-    def enumerate_paths(self,i):
-        k = i.get('Key')
-        if k[-1] != '/':
-            keys.append(k)
-        else:
-            dirs.append(k)
-        '''
+
     def download_thread(self,k):
         dest_pathname = os.path.join(self._local, k)
         os.makedirs(os.path.dirname(dest_pathname),exist_ok=True)
@@ -129,6 +126,22 @@ class Transfer():
         if self._is_verbose:
             print("{pid} is downloading {key} to {dir} \n".format(key=k,dir=dest_pathname,pid=process_name))
         boto3.client('s3').download_file(self._bucket_name, k, dest_pathname)
+        if (dest_pathname.endswith("tar.gz")):
+            if self._is_verbose:
+                print("{pid} is unzipping {key} to {dir} \n".format(key=dest_pathname,dir=dest_pathname.split(".tar.gz")[0],pid=process_name))
+            sys.stderr.write("\n")
+            tar = tarfile.open(dest_pathname, "r:gz", fileobj=ProgressFileObject(dest_pathname))
+            tar.extractall(os.path.dirname(dest_pathname))
+            tar.close()
+            os.remove(dest_pathname)
+        elif (dest_pathname.endswith(",tar")):
+            if self._is_verbose:
+                print("{pid} is unzipping {key} to {dir} \n".format(key=dest_pathname,dir=dest_pathname.split(",tar")[0],pid=process_name))
+            sys.stderr.write("\n")
+            tar = tarfile.open(dest_pathname, "r:")
+            tar.extractall(os.path.dirname(dest_pathname))
+            tar.close()
+            os.remove(dest_pathname)
 
     def percent_cb(self, complete, total):
         sys.stdout.write('.')
@@ -176,6 +189,34 @@ class Transfer():
                 #k.set_contents_from_filename(sourcepath,
                         #cb=percent_cb, num_cb=10)
                 pass
+
+def get_file_progress_file_object_class():
+    class FileProgressFileObject(tarfile.ExFileObject):
+        def read(self, size, *args):
+            on_progress(self.name, super.position, super.size)
+            return tarfile.ExFileObject.read(self, size, *args)
+    return FileProgressFileObject
+
+tarfile.TarFile.fileobject = get_file_progress_file_object_class()
+    
+def on_progress(filename, position, total_size):
+    print("%s: %d of %s" %(filename, position, total_size))
+
+class TestFileProgressFileObject(tarfile.ExFileObject):
+    def read(self, size, *args):
+        on_progress(self.name, super.position, super.size)
+        return tarfile.ExFileObject.read(self, size, *args)
+
+class ProgressFileObject(io.FileIO):
+    def __init__(self, path, *args, **kwargs):
+        self._total_size = os.path.getsize(path)
+        io.FileIO.__init__(self, path, *args, **kwargs)
+
+    def read(self, size):
+        sys.stderr.write("\rUnzipping file: {0:.2%}".format(self.tell()/self._total_size))
+        return io.FileIO.read(self, size)
+
+
 
 if __name__ == "__main__":
     args = get_parser().parse_args()
